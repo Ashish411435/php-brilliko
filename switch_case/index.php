e1fe07bc604f878bbe22d522acebe8bc7f336250
Ashish411435 <ash32623k@gmail.com>
2022-05-20 01:29:49 +0530

    Switch cases

---
+++ switch_case/index.php
@@ -0,0 +1,27 @@
+<?php
+    $age = 12;
+
+    
+    switch($age){
+        case 12:
+            echo "Your age is 12 <br>";
+            break;   // In switch case we need to use break tag otherwise it will execute the all files even though one condition is true.
+            
+        case 18:
+            echo "Your age is 18 <br>";
+            break;
+
+        case 24:
+            echo "Your age is 24 <br>";
+            break;
+
+        case 58: 
+            echo "Your age is 58 <br>";
+            break;
+
+        case 65:        
+            echo "Your age is 65 <br>";
+            break;
+            
+    }
+?>
