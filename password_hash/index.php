98d92ffc82e192e7fe339d2261f04f4c43eb9108
Ashish411435 <ash32623k@gmail.com>
2022-05-19 15:52:00 +0530

    Try

--- password_hash/index.php
+++ password_hash/index.php
@@ -1,5 +1,5 @@
 <?php
-$password = "hello@12345";
+$password = "ashish@12345";
 // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 // echo $hashed_password;
-echo password_verify($password,'$2y$10$QtN.LOBr9U1GVIwAyJmrT.ARZE8zx7f60wKTBbsH2JcN0JyRnoYwO');
+echo password_verify($password,'$2y$10$AVaEUkvDwxrbGKMJLFuSCOieT5FZkk6WyeEvgeB8x1ocnYxrGMXfC');
